365b1b414187b3365f97f3e20e1ee0daf4ed3c0c
Jerald Golden <jeraldgolden00@gmail.com>
2025-01-16 22:20:37 +0530

    Refactor multiplayer component to use original scene directly instead of cloning

--- src/multiplayer/multiplayers.tsx
+++ src/multiplayer/multiplayers.tsx
@@ -12,8 +12,7 @@ interface Player {
 }
 
 const MultiPlayers: React.FC = () => {
-    const { scene: originalScene }: any = useGLTF(Model);
-    const clonedScene = React.useMemo(() => originalScene.clone(), [originalScene]);
+    const { scene }: any = useGLTF(Model);
     const { room } = useRoom();
     const [players, setPlayers] = useState<Player[]>([]);
 
@@ -78,11 +77,11 @@ const MultiPlayers: React.FC = () => {
 
     return (
         <>
-            {clonedScene && players.map((player, index) => (
+            {scene && players.map((player, index) => (
                 <RigidBody key={index} includeInvisible lockRotations colliders={false} position={[...player.position]} mass={1} type="dynamic" rotation={[...player.rotation]}>
                     <CapsuleCollider args={[0.45, 0.75]} >
                         <group position={[0, -1.2, 0]} scale={[0.5, 0.5, 0.5]} rotation={[0, -Math.PI, 0]} >
-                            <primitive object={clonedScene} />
+                            <primitive object={scene} />
                         </group>
                     </CapsuleCollider>
                 </RigidBody>
